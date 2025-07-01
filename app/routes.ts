631e2ed72f4a9484df6fd8fa21c1e0ad16378476
JyotiKarri0506 <jyotikarri0506@gmail.com>
2025-07-01 17:08:13 -0400

    adding routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,13 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
+import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 // export default [index("routes/home.tsx")] satisfies RouteConfig;
 // this file is like configuration file where above routes file points to the main file which we wanna display like main page
 // instead of index lets use 2 additional methods
 // route and location of the file in route(path, file)
 export default [
-  route('dashboard', 'routes/admin/dashboard.tsx')
+  // its not a component but a method
+  layout("routes/admin/admin-layout.tsx", [
+    route("dashboard", "routes/admin/dashboard.tsx"),
+    route("all-users", "routes/admin/all-users.tsx"),
+  ]),
 ] satisfies RouteConfig;
