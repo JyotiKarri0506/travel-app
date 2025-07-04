98fd83053afcf9027c202663684e5a94a55ae1d7
JyotiKarri0506 <jyotikarri0506@gmail.com>
2025-07-03 21:13:50 -0400

    Updated TripsCard

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,6 +1,19 @@
-const TripCard = () => {
+import {Link, useLocation} from 'react-router';
+
+const TripCard = ({id, name, location, imageUrl, tags, price}: TripCardProps) => {
+
+    const path = useLocation();
+
   return (
-    <div>TripCard</div>
+    <Link to={path.pathname==='/' || path.pathname.startsWith('/travel') ? `/travel/${id}` : `/trips/${id}`} className='trip-card'>
+    <img src={imageUrl} alt={name}/>
+    <article>
+        <h2>{name}</h2>
+        <figure>
+            <img src='/assets/icons/location-mark.svg' alt='location' className='size-4'/>
+        </figure>
+    </article>
+    </Link>
   )
 }
 
